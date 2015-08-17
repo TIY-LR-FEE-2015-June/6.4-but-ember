db546b2eff2f94af53994d439eebca7153aa1b09
Ryan Tablada <ryan.tablada@gmail.com>
2015-08-17 16:13:08 -0500

    Comment Exercise C

--- app/routes/bookmarks/create.js
+++ app/routes/bookmarks/create.js
@@ -2,12 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
+    /**
+     * Create a new bookmark instance that we'll save to the server
+     */
     return this.store.createRecord('bookmark');
   },
 
   actions: {
+    /**
+     * Fired by the `save` action in the create template
+     * @param  {[type]} bookmark Bookmark model sent from the template action
+     */
     save: function(bookmark) {
       bookmark.save().then(() => {
+        /**
+         * Change to the bookmarks main route
+         */
         this.transitionTo('bookmarks');
       });
     }
--- app/routes/bookmarks/filtered.js
+++ app/routes/bookmarks/filtered.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
+    /**
+     * Grabs the models from the bookmarks route
+     * and filters it by the route parameter 'filter'
+     */
     return this.modelFor('bookmarks').filter(function(model) {
+      /**
+       * We need to use `model.get` since ember wraps model parameters
+       */
       return model.get('tag') === params.filter;
     });
   }
